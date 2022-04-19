c0d598e5f162f47e1ee920b6c4b010f6d6db20d3
herdal06 <h.erdal721@gmail.com>
2022-04-20 01:53:47 +0300

    Navigate with textView between login and register pages

--- app/src/main/java/com/herdal/roomdatabaseexercise/view/LoginFragment.kt
+++ app/src/main/java/com/herdal/roomdatabaseexercise/view/LoginFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.herdal.roomdatabaseexercise.R
+import kotlinx.android.synthetic.main.fragment_login.view.*
 
 
 class LoginFragment : Fragment() {
@@ -15,6 +17,12 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        view.textViewGoToSignUp.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_signUpFragment)
+        }
+
+        return view
     }
 }
--- app/src/main/java/com/herdal/roomdatabaseexercise/view/SignUpFragment.kt
+++ app/src/main/java/com/herdal/roomdatabaseexercise/view/SignUpFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.herdal.roomdatabaseexercise.R
+import kotlinx.android.synthetic.main.fragment_login.view.*
+import kotlinx.android.synthetic.main.fragment_sign_up.view.*
 
 
 class SignUpFragment : Fragment() {
@@ -15,6 +18,12 @@ class SignUpFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        val view = inflater.inflate(R.layout.fragment_sign_up, container, false)
+
+        view.textViewgoToLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_signUpFragment_to_loginFragment)
+        }
+
+        return view
     }
 }
